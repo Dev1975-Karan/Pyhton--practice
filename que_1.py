a702b07cbe884a8195cba3868f683c7efd56b152
Dev1975-Karan <devk101975@gmail.com>
2026-06-05 20:57:24 +0530

    Add solution for Question 001

---
+++ que_1.py
@@ -0,0 +1,7 @@
+#WAP to define the function to provide the factorial of no.
+def fact(x):
+    if x==0:
+        return 1
+    return x*fact(x-1)
+n=int(input("enter your number: "))
+print(fact(n))
